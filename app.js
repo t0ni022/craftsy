5fdfc5bf164f56cb4c8178f7e8ded270fb620c9c
Antonio <marco.a.falcon@outlook.com>
2023-12-02 18:59:50 -0300

    added swiper with error

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res)=> res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/profile',(req,res)=> res.sendFile(path.join(__dirname,'views','profile.html')))
 
 app.listen(PORT,()=> console.log(`server running in http://localhost:${PORT}`)) 
 
